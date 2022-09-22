1b020b330bae268aaf46301fcc9fd3a672eecdf5
Volkan Kelleci <kellecivolkan@gmail.com>
2022-09-22 08:23:07 +0300

    id 'kotlin-android-extensions' added

--- app/src/main/java/com/volkankelleci/bosstimeronmmogames/MainActivity.kt
+++ app/src/main/java/com/volkankelleci/bosstimeronmmogames/MainActivity.kt
@@ -2,10 +2,22 @@ package com.volkankelleci.bosstimeronmmogames
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
-class MainActivity : AppCompatActivity() {
+class MainActivity(millisInFuture: Long) : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        object : CountDownTimer(7200000 ,1000) {
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                TODO("Not yet implemented")
+            }
+
+        }
     }
 }
